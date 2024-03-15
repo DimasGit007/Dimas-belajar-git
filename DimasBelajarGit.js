5518c6111bf690117a8acdbe061d1ada92db30cc
DimasGit007 <dimascahyo659@gmail.com>
2024-03-15 08:45:31 +0700

    Membuat web Movie List dan Integrasi API dengan Axios

---
+++ DimasBelajarGit.js
@@ -0,0 +1,60 @@
+const getMovie = async () => {
+  const options = {
+    method: 'GET',
+    url: 'https://moviesverse1.p.rapidapi.com/get-trending-trailers',
+    headers: {
+      'X-RapidAPI-Key': 'ba2a226b2emsh56133bc0d959e0cp137bd0jsnc0ff6548f1a9',
+      'X-RapidAPI-Host': 'moviesverse1.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    
+    const trailerList = response.data && response.data.trailers
+
+    return trailerList 
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const trailerList = await getMovie()
+
+  const movieList =  document.getElementById('movieList')
+
+  trailerList.forEach((trailer) => {
+    const movieItem = document.createElement('li')
+    movieItem.className = 'movie-item'
+
+    const poster = document.createElement('img')
+    poster.src = trailer.image
+    poster.alt = trailer.title
+
+    const trailerDetail = document.createElement('div')
+    trailerDetail.className = 'movie-details'
+
+    const title = document.createElement('h2')
+    title.className = 'movie-title'
+    title.textContent = trailer.title
+
+    const released = document.createElement('p')
+    released.className = 'movie-year'
+    released.textContent = 'Release Date : ' + trailer.releaseDate;
+
+    const trailerLength = document.createElement('p')
+    trailerLength.className = 'movie-year'
+    trailerLength.textContent = 'Trailer Length : ' + trailer.trailerLength
+
+    trailerDetail.appendChild(title)
+    trailerDetail.appendChild(released)
+    trailerDetail.appendChild(trailerLength)
+    
+    movieItem.appendChild(poster)
+    movieItem.appendChild(trailerDetail)
+
+    movieList.appendChild(movieItem)
+  })
+})
